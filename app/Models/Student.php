fa2eb9b919c11c8917814953242f629be088059a
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-05-22 11:59:44 +0530

    feat:added student Model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'email',
+        'course',
+        'age',
+    ];
+
+    protected $casts = [   
+        'age' => 'integer',
+    ];
+
+    public function scopeSearch($query, $search)
+    //reusable query scope
+
+    {
+        return $query->where('name', 'like', "%{$search}%")
+            ->orWhere('email', 'like', "%{$search}%")
+            ->orWhere('course', 'like', "%{$search}%");
+    }
 }
