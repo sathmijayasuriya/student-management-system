77e5cb15408fb294f29b62ecfa6feb76397e16f9
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-06-16 05:41:38 +0530

    feat(auth): implement user login and registration functionality

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/favicon.png') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}">
+    <script src="{{ asset('assets/js/students.js') }}"></script>
+
+</head>
+
+<body>
+    <div
+        class="position-relative overflow-hidden text-bg-light min-vh-100 d-flex align-items-center justify-content-center">
+        <div class="d-flex align-items-center justify-content-center w-50">
+            <div class="col-md-8 col-lg-6 col-xxl-3">
+                <div class="card mb-0">
+                    <div class="card-body">
+                        <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                            <img src={{ asset('assets/images/logos/logoweb.png') }} alt="lgo" width="40"
+                                height="auto">
+                        </a>
+                        <h2 class="text-center">Sign In</h2>
+                        <form method="POST" action="/login">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Username</label>
+                                <input name="email" type="email" class="form-control" id="email"
+                                    value="{{ old('email') }}" aria-describedby="emailHelp" required>
+                            </div>
+
+                            <div class="mb-4">
+                                <label for="password" class="form-label">Password</label>
+                                <input name="password" type="password" class="form-control" id="password" required>
+                            </div>
+                            @error('email')
+                                <p class="text-danger">{{ $message }}</p>
+                            @enderror
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <div class="form-check">
+                                    <input class="form-check-input primary" type="checkbox" name="remember" value="1"
+                                        id="flexCheckChecked" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="form-check-label text-dark" for="flexCheckChecked">
+                                        Remeber Me
+                                    </label>    
+                                </div>
+                                <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
+                            </div>
+                            <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign
+                                In</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="./assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="./assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/js/sidebarmenu.js"></script>
+    <script src="./assets/js/app.min.js"></script>
+    <script src="./assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+    <script src="./assets/libs/simplebar/dist/simplebar.js"></script>
+    <script src="./assets/js/dashboard.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+
+</body>
+
+</html>
